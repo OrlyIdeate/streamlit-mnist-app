f7f948bfcd7e98505b04927b94134ce56f86a248
OrlyIdeate <ueno.jin25@outlook.jp>
2025-02-04 14:17:31 +0900

    全結合層のユニット数を変更して、モデルのパラメータを削減

--- src/app.py
+++ src/app.py
@@ -16,8 +16,8 @@ class CNN(nn.Module):
         self.conv3 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
         self.conv4 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
-        self.fc1 = nn.Linear(64, 128)
-        self.fc2 = nn.Linear(128, 10)
+        self.fc1 = nn.Linear(64, 32)
+        self.fc2 = nn.Linear(32, 10)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
